0bf74bdf6c84f5ab6f7f57232478b7c4c22e7481
prajina-Adhikari <aprajina45@gmail.com>
2026-09-15 10:55:05 +0545

    added admin panel

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="app-shell">
+      <Navbar />
+      <Routes>
+       
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
