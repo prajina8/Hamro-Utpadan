017f23c56d1731a8aef2ccd2f928d87fe6883b83
prajina-Adhikari <aprajina45@gmail.com>
2026-09-15 16:38:28 +0545

    changes in admin panel

---
+++ admin/src/api/axios.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("hu_admin_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("hu_admin_token");
+      localStorage.removeItem("hu_admin_user");
+      if (!window.location.pathname.includes("/login")) {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
