be1900fab8613aacb63eacb4dcede728f45f94ad
prajina-Adhikari <aprajina45@gmail.com>
2026-09-15 20:25:31 +0545

    new update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-
+import Navbar from "./components/Navbar.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ManageUsers from "./pages/ManageUsers.jsx";
 
 function App() {
   return (
     <div className="app-shell">
       <Navbar />
       <Routes>
-       
+        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/users" element={<ProtectedRoute><ManageUsers /></ProtectedRoute>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
